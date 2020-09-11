a5a888fb425f2c6550af805b370722f66999545d
Vincent Sanders <vincesanders@hotmail.com>
2020-09-11 18:24:10 -0500

    Added the set board case to the reducer and added the corresponding action to the actions file.

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,9 +1,27 @@
+import { SET_BOARD } from '../actions/actions';
+
 export const initialState = {
-    cards: []
+    board: []
 }
 
-const reducer = (state, action) => {
+const reducer = (state=initialState, action) => {
     switch (action.type) {
+        case SET_BOARD:
+            return {
+                ...state,
+                board: action.payload.map((value, index) => {
+                    const card = {
+                        id: index,
+                        value,
+                        flipped: false,
+                        selected: false
+                    };
+                    if (index === 0) {
+                        card.selected === true;
+                    }
+                    return card;
+                })
+            }
         default:
             return state;
     }
